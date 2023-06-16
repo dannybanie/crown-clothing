4eae2c89f75a1cbc74ff112e0f0211a9b841b6fd
dannybanie <danielbrooks0000@gmail.com>
2023-06-16 23:58:16 +0100

    updated files for directory and categories

--- src/component/categories/categories-container/categories-container.component.jsx
+++ src/component/categories/categories-container/categories-container.component.jsx
@@ -4,7 +4,7 @@ import './categories-container.styles.scss';
 export default function Category({category}){ 
     const { id, title, imageUrl } =  category;
     return(   
-            <Card title={title} imageUrl={imageUrl} key={id}/> 
+            <Card title={title} imageUrl={imageUrl} key={id} classname='one'/> 
     );
 
 }
